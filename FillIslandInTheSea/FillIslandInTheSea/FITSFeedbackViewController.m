446b723ba3fc69a174e96fa894fdf64870bfe0cf
runingfish <1826931541@qq.com>
2025-06-02 11:46:31 +0800

    意见反馈页面

--- FillIslandInTheSea/FillIslandInTheSea/FITSFeedbackViewController.m
+++ FillIslandInTheSea/FillIslandInTheSea/FITSFeedbackViewController.m
@@ -6,26 +6,39 @@
 //
 
 #import "FITSFeedbackViewController.h"
-
-@interface FITSFeedbackViewController ()
-
-@end
-
 @implementation FITSFeedbackViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"意见反馈";
+    self.view.backgroundColor = MAIN_VIEW_COLOR;
+    
+    UILabel *fits_feedback_title_label = [[UILabel alloc] initWithFrame:CGRectMake(10, 110, self.view.frame.size.width-20, 40)];
+    [self.view addSubview:fits_feedback_title_label];
+    fits_feedback_title_label.text = @"请填写您的意见或建议";
+    fits_feedback_title_label.textColor = [UIColor blackColor];
+    fits_feedback_title_label.textAlignment = NSTextAlignmentLeft;
+    fits_feedback_title_label.font = [UIFont systemFontOfSize:16];
+    
+    UITextView *fits_feedback_textView = [[UITextView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(fits_feedback_title_label.frame) + 10, self.view.frame.size.width-20, 180)];
+    [self.view addSubview:fits_feedback_textView];
+    fits_feedback_textView.layer.borderWidth = 1;
+    fits_feedback_textView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    fits_feedback_textView.layer.cornerRadius = 5;
+    fits_feedback_textView.layer.masksToBounds = YES;
+    
+    UIButton *fits_feedback_confirmButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:fits_feedback_confirmButton];
+    fits_feedback_confirmButton.frame = CGRectMake(10, CGRectGetMaxY(fits_feedback_textView.frame)+37, [UIScreen mainScreen].bounds.size.width-20, 44);
+    [fits_feedback_confirmButton setTitle:@"提交反馈" forState:UIControlStateNormal];
+    [fits_feedback_confirmButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    fits_feedback_confirmButton.titleLabel.font = [UIFont systemFontOfSize:20];
+    [fits_feedback_confirmButton addTarget:self action:@selector(fits_feedback_confirmButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    fits_feedback_confirmButton.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1];;
+    fits_feedback_confirmButton.layer.cornerRadius = 10;
+    fits_feedback_confirmButton.layer.masksToBounds = YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)fits_feedback_confirmButtonClick:(UIButton *)button{
+    [self.navigationController popViewControllerAnimated:YES];
 }
-*/
-
 @end
